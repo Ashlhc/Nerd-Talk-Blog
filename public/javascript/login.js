1421855ea6a68bb0f22aead7bac7a7905035f5fb
Ashleigh Chute <ashlhc@uw.edu>
2023-05-17 10:11:49 -0700

    added views

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,32 +1,4 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();  
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      //check if the response status
-      if (response.ok) {
-        console.log('success');
-        alert('New user created you can now log in');
-        document.location.reload();
-      } else {
-        alert(response.statusText);
-      }
-    }  
-  }
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-  
-  async function loginFormHandler(event) {
+async function loginFormHandler(event) {
     event.preventDefault();
   
     const username = document.querySelector('#username-login').value.trim();
---
+++ public/javascript/signup.js
@@ -0,0 +1,27 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();  
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      //check if the response status
+      if (response.ok) {
+        console.log('success');
+        alert('New user created you can now log in');
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }  
+  }
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
