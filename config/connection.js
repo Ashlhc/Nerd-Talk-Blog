b4495ed9ff221f81e4fbda656b934e59aa145340
Ashlhc <128103621+Ashlhc@users.noreply.github.com>
2023-05-29 19:53:46 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSBD_URL) {
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
       port: 3306
     }
